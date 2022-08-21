2485576e5c0a35da0e5f5581c12bacb975e05e75
Daniel Kubiak <kubiakdaniel97@gmail.com>
2022-08-21 17:18:26 +0200

    create logic behind adding player's name

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,23 @@
+const players = [
+    {
+        name:'',
+        symbol:'O'
+    },
+    {
+        name:'',
+        symbol:'X'
+    }
+]
+
+// let activePlayer = 0;
+// let editedPlayer = 0;
+// let currentRound = 1;
+
 const changeNameIcon = document.getElementsByClassName('material-symbols-rounded');
 const nameInput = document.getElementsByClassName('player-name-input');
+const formElements = document.querySelectorAll('form');
+const saveNameBtn = document.getElementsByClassName('player-name-btn');
+const errorOutput = document.getElementsByClassName('config-error');
 
 const startNewGameBtn = document.getElementById('start-game-btn');
 const gameBoard = document.getElementById('game-board');
@@ -7,5 +25,7 @@ const gameBoard = document.getElementById('game-board');
 for (const icon of changeNameIcon) {
     icon.addEventListener('click', enableUserInput);
 }
-
+for (const form of formElements) {
+    form.addEventListener('submit', savePlayerName)
+}
 startNewGameBtn.addEventListener('click', showGameBoard);
--- scripts/configuration.js
+++ scripts/configuration.js
@@ -1,13 +1,45 @@
 function enableUserInput(event){
 const editIcon = event.target;
 const h3Element = editIcon.parentElement;
-const userInput = h3Element.nextElementSibling;
-const saveBtn = userInput.nextElementSibling;
+const inputElementDiv = h3Element.nextElementSibling;
+const saveBtn = inputElementDiv.nextElementSibling;
 
 h3Element.style.display ='none';
-userInput.style.display = 'block';
+inputElementDiv.style.display = 'block';
 saveBtn.style.display = 'block';
 };
+
+function savePlayerName(event){
+    const selectedPlayerId = +event.target.dataset.playerid;
+    const formElement = event.target;
+    const userName = formElement.children[0].lastElementChild;
+    const h3Element = formElement.children[0];
+    const inputElementDiv = formElement.children[1];
+    const inputElement = formElement.children[1].children[0];
+    const errorMessage = formElement.children[1].children[1];
+    const saveBtn = formElement.children[2];
+
+    
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayerName = formData.get('username').trim();
+
+    if(!enteredPlayerName){
+        inputElement.classList.add('config-error');
+        errorMessage.classList.add('error');
+        errorMessage.innerText = 'Enter a valid name!'
+        return
+    }
+
+    players[selectedPlayerId-1].name = enteredPlayerName;
+    userName.innerText = enteredPlayerName;
+
+    userName.style.display = 'inline';
+    h3Element.style.display = 'block';
+    saveBtn.style.display = 'none';
+    inputElementDiv.style.display = 'none';
+
+}
 
 function showGameBoard(){
     gameBoard.style.display = 'grid'
