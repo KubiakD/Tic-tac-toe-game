c78a2dfde4b238be70ed7da17ed31659b6918efa
Daniel Kubiak <kubiakdaniel97@gmail.com>
2022-08-25 19:12:05 +0200

    add tracking for winner

--- scripts/game.js
+++ scripts/game.js
@@ -21,3 +21,33 @@ function selectField(event) {
     }
     return
 }
+
+function checkForWinner() {
+    for (let i=0; i<3; i++) {
+        if (gameBoardData[i][0] > 0 &&
+            gameBoardData[i][0] === gameBoardData[i][1] &&
+            gameBoardData[i][1] === gameBoardData[i][2]) {
+            return gameBoardData[i][0];
+        }
+    } 
+    for (let i=0; i<3; i++) {
+        if (gameBoardData[0][i] > 0 &&
+            gameBoardData[0][i] === gameBoardData[1][i] &&
+            gameBoardData[1][i] === gameBoardData[2][i]) {
+            return gameBoardData[0][i];
+
+        }
+    } 
+    if (gameBoardData[0][0] > 0 &&
+        gameBoardData[0][0] === gameBoardData[1][1] &&
+        gameBoardData[1][1] === gameBoardData[2][2]) {
+        return gameBoardData[0][0]
+
+    }
+    if (gameBoardData[0][2] > 0 &&
+        gameBoardData[0][2] === gameBoardData[1][1] &&
+        gameBoardData[1][1] === gameBoardData[2][0]) {
+        return gameBoardData[0][2]
+    }
+    
+}
